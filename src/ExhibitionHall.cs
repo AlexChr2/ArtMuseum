1c3e0c5b8cf269cc27696b91d52aa6efe61e5bb0
ConstArt <k.artemiou@outlook.com>
2024-01-22 23:17:33 +0200

    Hierarchical Structure

---
+++ src/ExhibitionHall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ergasia3.src
+{
+	public partial class ExhibitionHall : Form
+	{
+		public ExhibitionHall()
+		{
+			InitializeComponent();
+		}
+	}
+}
