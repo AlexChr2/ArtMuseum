84279ea702f38f0c375d4b413c6bcaee00e3d4c2
ConstArt <k.artemiou@outlook.com>
2024-01-25 18:32:05 +0200

    Updated some things in Cafeteria form

--- src/Cinema/Cafeteria.Designer.cs
+++ src/Cinema/Cafeteria.Designer.cs
@@ -29,310 +29,60 @@
 		private void InitializeComponent()
 		{
 			this.panel1 = new Panel();
-			this.button1 = new Button();
-			this.panel2 = new Panel();
-			this.panel7 = new Panel();
-			this.label9 = new Label();
-			this.label10 = new Label();
-			this.panel5 = new Panel();
-			this.label5 = new Label();
-			this.label6 = new Label();
-			this.panel8 = new Panel();
-			this.label11 = new Label();
-			this.label12 = new Label();
-			this.panel4 = new Panel();
-			this.label3 = new Label();
-			this.label4 = new Label();
-			this.panel6 = new Panel();
-			this.label7 = new Label();
-			this.label8 = new Label();
-			this.panel3 = new Panel();
-			this.label2 = new Label();
 			this.label1 = new Label();
+			this.panel2 = new Panel();
 			this.panel1.SuspendLayout();
-			this.panel2.SuspendLayout();
-			this.panel7.SuspendLayout();
-			this.panel5.SuspendLayout();
-			this.panel8.SuspendLayout();
-			this.panel4.SuspendLayout();
-			this.panel6.SuspendLayout();
-			this.panel3.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// panel1
 			// 
-			this.panel1.BackColor = SystemColors.GradientActiveCaption;
-			this.panel1.Controls.Add( this.button1 );
+			this.panel1.BackColor = Color.MediumSlateBlue;
 			this.panel1.Controls.Add( this.panel2 );
-			this.panel1.Location = new Point( 12, 12 );
+			this.panel1.Location = new Point( 5, 76 );
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new Size( 719, 708 );
+			this.panel1.Size = new Size( 533, 583 );
 			this.panel1.TabIndex = 2;
-			// 
-			// button1
-			// 
-			this.button1.Location = new Point( 265, 615 );
-			this.button1.Name = "button1";
-			this.button1.Size = new Size( 181, 65 );
-			this.button1.TabIndex = 10;
-			this.button1.Text = "Purchase";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// panel2
-			// 
-			this.panel2.BackColor = SystemColors.ControlLight;
-			this.panel2.Controls.Add( this.panel7 );
-			this.panel2.Controls.Add( this.panel5 );
-			this.panel2.Controls.Add( this.panel8 );
-			this.panel2.Controls.Add( this.panel4 );
-			this.panel2.Controls.Add( this.panel6 );
-			this.panel2.Controls.Add( this.panel3 );
-			this.panel2.Location = new Point( 3, 3 );
-			this.panel2.Name = "panel2";
-			this.panel2.Size = new Size( 712, 583 );
-			this.panel2.TabIndex = 9;
-			// 
-			// panel7
-			// 
-			this.panel7.BackColor = SystemColors.ButtonFace;
-			this.panel7.Controls.Add( this.label9 );
-			this.panel7.Controls.Add( this.label10 );
-			this.panel7.Location = new Point( 408, 391 );
-			this.panel7.Name = "panel7";
-			this.panel7.Size = new Size( 281, 169 );
-			this.panel7.TabIndex = 14;
-			// 
-			// label9
-			// 
-			this.label9.AutoSize = true;
-			this.label9.BackColor = SystemColors.Control;
-			this.label9.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label9.Location = new Point( 45, 113 );
-			this.label9.Name = "label9";
-			this.label9.Size = new Size( 198, 28 );
-			this.label9.TabIndex = 8;
-			this.label9.Text = "NumberOfFoodItems";
-			// 
-			// label10
-			// 
-			this.label10.AutoSize = true;
-			this.label10.BackColor = SystemColors.Control;
-			this.label10.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label10.Location = new Point( 80, 24 );
-			this.label10.Name = "label10";
-			this.label10.Size = new Size( 118, 28 );
-			this.label10.TabIndex = 7;
-			this.label10.Text = "FoodPicture";
-			// 
-			// panel5
-			// 
-			this.panel5.BackColor = SystemColors.ButtonFace;
-			this.panel5.Controls.Add( this.label5 );
-			this.panel5.Controls.Add( this.label6 );
-			this.panel5.Location = new Point( 408, 202 );
-			this.panel5.Name = "panel5";
-			this.panel5.Size = new Size( 281, 169 );
-			this.panel5.TabIndex = 12;
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.BackColor = SystemColors.Control;
-			this.label5.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label5.Location = new Point( 45, 113 );
-			this.label5.Name = "label5";
-			this.label5.Size = new Size( 198, 28 );
-			this.label5.TabIndex = 8;
-			this.label5.Text = "NumberOfFoodItems";
-			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.BackColor = SystemColors.Control;
-			this.label6.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label6.Location = new Point( 80, 24 );
-			this.label6.Name = "label6";
-			this.label6.Size = new Size( 118, 28 );
-			this.label6.TabIndex = 7;
-			this.label6.Text = "FoodPicture";
-			// 
-			// panel8
-			// 
-			this.panel8.BackColor = SystemColors.ButtonFace;
-			this.panel8.Controls.Add( this.label11 );
-			this.panel8.Controls.Add( this.label12 );
-			this.panel8.Location = new Point( 22, 391 );
-			this.panel8.Name = "panel8";
-			this.panel8.Size = new Size( 281, 169 );
-			this.panel8.TabIndex = 13;
-			// 
-			// label11
-			// 
-			this.label11.AutoSize = true;
-			this.label11.BackColor = SystemColors.Control;
-			this.label11.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label11.Location = new Point( 45, 113 );
-			this.label11.Name = "label11";
-			this.label11.Size = new Size( 198, 28 );
-			this.label11.TabIndex = 8;
-			this.label11.Text = "NumberOfFoodItems";
-			// 
-			// label12
-			// 
-			this.label12.AutoSize = true;
-			this.label12.BackColor = SystemColors.Control;
-			this.label12.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label12.Location = new Point( 80, 24 );
-			this.label12.Name = "label12";
-			this.label12.Size = new Size( 118, 28 );
-			this.label12.TabIndex = 7;
-			this.label12.Text = "FoodPicture";
-			// 
-			// panel4
-			// 
-			this.panel4.BackColor = SystemColors.ButtonFace;
-			this.panel4.Controls.Add( this.label3 );
-			this.panel4.Controls.Add( this.label4 );
-			this.panel4.Location = new Point( 408, 16 );
-			this.panel4.Name = "panel4";
-			this.panel4.Size = new Size( 281, 169 );
-			this.panel4.TabIndex = 10;
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.BackColor = SystemColors.Control;
-			this.label3.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label3.Location = new Point( 45, 113 );
-			this.label3.Name = "label3";
-			this.label3.Size = new Size( 198, 28 );
-			this.label3.TabIndex = 8;
-			this.label3.Text = "NumberOfFoodItems";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.BackColor = SystemColors.Control;
-			this.label4.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label4.Location = new Point( 80, 24 );
-			this.label4.Name = "label4";
-			this.label4.Size = new Size( 118, 28 );
-			this.label4.TabIndex = 7;
-			this.label4.Text = "FoodPicture";
-			// 
-			// panel6
-			// 
-			this.panel6.BackColor = SystemColors.ButtonFace;
-			this.panel6.Controls.Add( this.label7 );
-			this.panel6.Controls.Add( this.label8 );
-			this.panel6.Location = new Point( 22, 202 );
-			this.panel6.Name = "panel6";
-			this.panel6.Size = new Size( 281, 169 );
-			this.panel6.TabIndex = 11;
-			// 
-			// label7
-			// 
-			this.label7.AutoSize = true;
-			this.label7.BackColor = SystemColors.Control;
-			this.label7.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label7.Location = new Point( 45, 113 );
-			this.label7.Name = "label7";
-			this.label7.Size = new Size( 198, 28 );
-			this.label7.TabIndex = 8;
-			this.label7.Text = "NumberOfFoodItems";
-			// 
-			// label8
-			// 
-			this.label8.AutoSize = true;
-			this.label8.BackColor = SystemColors.Control;
-			this.label8.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label8.Location = new Point( 80, 24 );
-			this.label8.Name = "label8";
-			this.label8.Size = new Size( 118, 28 );
-			this.label8.TabIndex = 7;
-			this.label8.Text = "FoodPicture";
-			// 
-			// panel3
-			// 
-			this.panel3.BackColor = SystemColors.ButtonFace;
-			this.panel3.Controls.Add( this.label2 );
-			this.panel3.Controls.Add( this.label1 );
-			this.panel3.Location = new Point( 22, 16 );
-			this.panel3.Name = "panel3";
-			this.panel3.Size = new Size( 281, 169 );
-			this.panel3.TabIndex = 9;
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.BackColor = SystemColors.Control;
-			this.label2.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label2.Location = new Point( 45, 113 );
-			this.label2.Name = "label2";
-			this.label2.Size = new Size( 198, 28 );
-			this.label2.TabIndex = 8;
-			this.label2.Text = "NumberOfFoodItems";
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.BackColor = SystemColors.Control;
-			this.label1.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label1.Location = new Point( 80, 24 );
+			this.label1.FlatStyle = FlatStyle.Flat;
+			this.label1.Font = new Font( "Lucida Bright", 22.2F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label1.ForeColor = Color.SeaShell;
+			this.label1.Location = new Point( 182, 9 );
 			this.label1.Name = "label1";
-			this.label1.Size = new Size( 118, 28 );
-			this.label1.TabIndex = 7;
-			this.label1.Text = "FoodPicture";
+			this.label1.Size = new Size( 184, 42 );
+			this.label1.TabIndex = 45;
+			this.label1.Text = "Cafeteria";
+			this.label1.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// panel2
+			// 
+			this.panel2.Location = new Point( 7, 3 );
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new Size( 521, 362 );
+			this.panel2.TabIndex = 0;
 			// 
 			// Cafeteria
 			// 
 			AutoScaleDimensions = new SizeF( 8F, 20F );
 			AutoScaleMode = AutoScaleMode.Font;
 			BackColor = Color.DarkSlateBlue;
-			ClientSize = new Size( 743, 733 );
+			ClientSize = new Size( 541, 733 );
+			Controls.Add( this.label1 );
 			Controls.Add( this.panel1 );
 			FormBorderStyle = FormBorderStyle.FixedToolWindow;
 			Name = "Cafeteria";
 			StartPosition = FormStartPosition.CenterScreen;
 			this.panel1.ResumeLayout( false );
-			this.panel2.ResumeLayout( false );
-			this.panel7.ResumeLayout( false );
-			this.panel7.PerformLayout();
-			this.panel5.ResumeLayout( false );
-			this.panel5.PerformLayout();
-			this.panel8.ResumeLayout( false );
-			this.panel8.PerformLayout();
-			this.panel4.ResumeLayout( false );
-			this.panel4.PerformLayout();
-			this.panel6.ResumeLayout( false );
-			this.panel6.PerformLayout();
-			this.panel3.ResumeLayout( false );
-			this.panel3.PerformLayout();
 			this.ResumeLayout( false );
+			this.PerformLayout();
 		}
 
 		#endregion
 
 		private Panel panel1;
-		private Panel panel2;
-		private Panel panel5;
-		private Label label5;
-		private Label label6;
-		private Panel panel4;
-		private Label label3;
-		private Label label4;
-		private Panel panel6;
-		private Label label7;
-		private Label label8;
-		private Panel panel3;
-		private Label label2;
 		private Label label1;
-		private Button button1;
-		private Panel panel7;
-		private Label label9;
-		private Label label10;
-		private Panel panel8;
-		private Label label11;
-		private Label label12;
+		private Panel panel2;
 	}
 }
