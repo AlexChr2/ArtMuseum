e1ca068b033a7db099ccc2a9cff57531a6753a51
Al-Christodoulou <99661503+Al-Christodoulou@users.noreply.github.com>
2024-01-25 20:28:02 +0200

    added code for reading the gallery xml in MatterHall

--- src/ExhibitionHall/HallSelection.cs
+++ src/ExhibitionHall/HallSelection.cs
@@ -10,6 +10,13 @@ using System.Windows.Forms;
 
 namespace Ergasia3.src.ExhibitionHall
 {
+	public enum SelectionCategory
+	{
+		Art = 0,
+		Music,
+		Movies
+	}
+
 	public partial class HallSelection : Form
 	{
 		public HallSelection()
--- src/ExhibitionHall/MatterHall.cs
+++ src/ExhibitionHall/MatterHall.cs
@@ -7,14 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Ergasia3.src.ExhibitionHall
 {
 	public partial class MatterHall : Form
 	{
+		private const string XMLPath = "Data/gallery.xml";
+		private readonly List<PresentationContents> presentations = [];
+
+		private readonly struct PresentationContents
+		{
+			private readonly string imagePath;
+			private readonly string info;
+
+			public PresentationContents(string imagePath, string info)
+			{
+				this.imagePath = imagePath;
+				this.info = info;
+			}
+		}
+
 		public MatterHall()
 		{
 			InitializeComponent();
+		}
+
+		private void MatterHall_Shown(object sender, EventArgs e)
+		{
+			// temporary, this should be passed as a parameter from HallSelection
+			readGalleryFile(SelectionCategory.Music);
+		}
+
+		private void readGalleryFile(SelectionCategory category)
+		{
+			XmlDocument doc = new();
+			try
+			{
+				doc.Load(XMLPath);
+				XmlNode? rootNode = doc.SelectSingleNode("gallery");
+				if (rootNode == null)
+					throw new Exception();
+
+				string nodeSelection = "";
+				switch (category)
+				{
+					case SelectionCategory.Art:
+						nodeSelection = "painting";
+						break;
+					case SelectionCategory.Music:
+						nodeSelection = "music";
+						break;
+					case SelectionCategory.Movies:
+						nodeSelection = "movies";
+						break;
+				}
+
+				XmlNode? presContents = rootNode.SelectSingleNode(nodeSelection);
+				if (presContents == null)
+					throw new Exception();
+
+				List<PresentationContents> presentationContents = [];
+				foreach (XmlNode node in presContents.ChildNodes)
+				{
+					XmlNode? imagePath = node.SelectSingleNode("image");
+					XmlNode? info = node.SelectSingleNode("info");
+
+					if (imagePath == null ||
+						info == null ||
+						imagePath.Attributes["path"] == null
+					)
+						continue;
+					else
+					{
+						// replace endlines with a space and remove \t
+						string infoText = info.InnerText.Replace(
+							Environment.NewLine, " ");
+						infoText = infoText.Replace("\t", "");
+						presentationContents.Add(new PresentationContents(
+							imagePath.Attributes["path"].Value,
+							infoText
+						));
+					}
+				}
+			}
+			catch (Exception)
+			{
+				// TODO: what to show on-screen in this case?
+				return;
+			}
+
 		}
 	}
 }
