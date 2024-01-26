f2c1dbf9590b782e2b255c0a4403e3b35989efd1
ConstArt <k.artemiou@outlook.com>
2024-01-26 21:55:13 +0200

    Update DeviceManager form's structure

--- src/DeviceManager.Designer.cs
+++ src/DeviceManager.Designer.cs
@@ -29,116 +29,421 @@
 		private void InitializeComponent()
 		{
 			this.panel1 = new Panel();
-			this.label1 = new Label();
+			this.panel4 = new Panel();
+			this.panel13 = new Panel();
+			this.label15 = new Label();
+			this.panel3 = new Panel();
+			this.panel9 = new Panel();
+			this.button2 = new Button();
+			this.panel10 = new Panel();
 			this.label2 = new Label();
-			this.label3 = new Label();
-			this.label4 = new Label();
-			this.label5 = new Label();
+			this.panel7 = new Panel();
+			this.button1 = new Button();
+			this.panel8 = new Panel();
+			this.label1 = new Label();
+			this.panel5 = new Panel();
+			this.button4 = new Button();
+			this.panel6 = new Panel();
+			this.label9 = new Label();
+			this.panel2 = new Panel();
+			this.panel11 = new Panel();
 			this.label6 = new Label();
+			this.label5 = new Label();
+			this.button6 = new Button();
+			this.button5 = new Button();
+			this.label4 = new Label();
+			this.button3 = new Button();
+			this.label3 = new Label();
+			this.vScrollBar1 = new VScrollBar();
+			this.pictureBox1 = new PictureBox();
 			this.panel1.SuspendLayout();
+			this.panel4.SuspendLayout();
+			this.panel13.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.panel9.SuspendLayout();
+			this.panel7.SuspendLayout();
+			this.panel5.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel11.SuspendLayout();
+			(( System.ComponentModel.ISupportInitialize )this.pictureBox1).BeginInit();
 			this.SuspendLayout();
 			// 
 			// panel1
 			// 
-			this.panel1.BackColor = SystemColors.GradientActiveCaption;
-			this.panel1.Controls.Add( this.label5 );
-			this.panel1.Controls.Add( this.label6 );
-			this.panel1.Controls.Add( this.label3 );
-			this.panel1.Controls.Add( this.label4 );
-			this.panel1.Controls.Add( this.label2 );
-			this.panel1.Controls.Add( this.label1 );
-			this.panel1.Location = new Point( 12, 12 );
+			this.panel1.BackColor = Color.MediumSlateBlue;
+			this.panel1.Controls.Add( this.panel4 );
+			this.panel1.Controls.Add( this.panel3 );
+			this.panel1.Controls.Add( this.panel2 );
+			this.panel1.Location = new Point( 2, 3 );
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new Size( 512, 586 );
+			this.panel1.Size = new Size( 462, 719 );
 			this.panel1.TabIndex = 0;
 			// 
-			// label1
+			// panel4
 			// 
-			this.label1.AutoSize = true;
-			this.label1.BackColor = SystemColors.ButtonFace;
-			this.label1.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label1.Location = new Point( 36, 40 );
-			this.label1.Name = "label1";
-			this.label1.Size = new Size( 26, 168 );
-			this.label1.TabIndex = 0;
-			this.label1.Text = "L\r\nI\r\nG\r\nH\r\nT\r\nS";
+			this.panel4.BackColor = Color.DarkSlateBlue;
+			this.panel4.Controls.Add( this.panel13 );
+			this.panel4.Location = new Point( 10, 486 );
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new Size( 443, 222 );
+			this.panel4.TabIndex = 2;
+			// 
+			// panel13
+			// 
+			this.panel13.BackColor = Color.MediumSlateBlue;
+			this.panel13.Controls.Add( this.pictureBox1 );
+			this.panel13.Controls.Add( this.vScrollBar1 );
+			this.panel13.Controls.Add( this.label15 );
+			this.panel13.Location = new Point( 3, 3 );
+			this.panel13.Name = "panel13";
+			this.panel13.Size = new Size( 437, 215 );
+			this.panel13.TabIndex = 64;
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.FlatStyle = FlatStyle.Flat;
+			this.label15.Font = new Font( "Lucida Bright", 16.2F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label15.ForeColor = Color.SeaShell;
+			this.label15.Location = new Point( 19, 88 );
+			this.label15.Name = "label15";
+			this.label15.Size = new Size( 99, 31 );
+			this.label15.TabIndex = 58;
+			this.label15.Text = "Audio";
+			this.label15.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = Color.DarkSlateBlue;
+			this.panel3.Controls.Add( this.panel9 );
+			this.panel3.Controls.Add( this.panel7 );
+			this.panel3.Controls.Add( this.panel5 );
+			this.panel3.Location = new Point( 10, 236 );
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new Size( 443, 244 );
+			this.panel3.TabIndex = 1;
+			// 
+			// panel9
+			// 
+			this.panel9.BackColor = Color.MediumSlateBlue;
+			this.panel9.Controls.Add( this.button2 );
+			this.panel9.Controls.Add( this.panel10 );
+			this.panel9.Controls.Add( this.label2 );
+			this.panel9.Location = new Point( 296, 3 );
+			this.panel9.Name = "panel9";
+			this.panel9.Size = new Size( 144, 238 );
+			this.panel9.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = Color.DarkSlateBlue;
+			this.button2.Font = new Font( "Lucida Calligraphy", 10.8F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.button2.ForeColor = Color.Snow;
+			this.button2.Location = new Point( 27, 172 );
+			this.button2.Name = "button2";
+			this.button2.Size = new Size( 91, 38 );
+			this.button2.TabIndex = 57;
+			this.button2.Text = "Off";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// panel10
+			// 
+			this.panel10.BackColor = Color.DarkSlateBlue;
+			this.panel10.Location = new Point( 27, 76 );
+			this.panel10.Name = "panel10";
+			this.panel10.Size = new Size( 91, 63 );
+			this.panel10.TabIndex = 56;
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.BackColor = SystemColors.ButtonFace;
-			this.label2.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label2.Location = new Point( 108, 66 );
+			this.label2.FlatStyle = FlatStyle.Flat;
+			this.label2.Font = new Font( "Lucida Bright", 13.8F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label2.ForeColor = Color.SeaShell;
+			this.label2.Location = new Point( 27, 26 );
 			this.label2.Name = "label2";
-			this.label2.Size = new Size( 337, 112 );
-			this.label2.TabIndex = 1;
-			this.label2.Text = "Here we will probably place some\r\ngradient for the lights of the museum\r\nin order to create an aesthetic \r\natmosphere.";
+			this.label2.Size = new Size( 91, 26 );
+			this.label2.TabIndex = 55;
+			this.label2.Text = "Light 3";
+			this.label2.TextAlign = ContentAlignment.MiddleCenter;
 			// 
-			// label3
+			// panel7
 			// 
-			this.label3.AutoSize = true;
-			this.label3.BackColor = SystemColors.ButtonFace;
-			this.label3.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label3.Location = new Point( 108, 280 );
-			this.label3.Name = "label3";
-			this.label3.Size = new Size( 364, 84 );
-			this.label3.TabIndex = 3;
-			this.label3.Text = "Here we will probably place a numerical\r\nbar, which will contain the vocal range of\r\nthe overall audio inside the museum.";
+			this.panel7.BackColor = Color.MediumSlateBlue;
+			this.panel7.Controls.Add( this.button1 );
+			this.panel7.Controls.Add( this.panel8 );
+			this.panel7.Controls.Add( this.label1 );
+			this.panel7.Location = new Point( 148, 3 );
+			this.panel7.Name = "panel7";
+			this.panel7.Size = new Size( 147, 238 );
+			this.panel7.TabIndex = 2;
 			// 
-			// label4
+			// button1
 			// 
-			this.label4.AutoSize = true;
-			this.label4.BackColor = SystemColors.ButtonFace;
-			this.label4.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label4.Location = new Point( 36, 250 );
-			this.label4.Name = "label4";
-			this.label4.Size = new Size( 27, 140 );
-			this.label4.TabIndex = 2;
-			this.label4.Text = "A\r\nU\r\nD\r\nI\r\nO";
+			this.button1.BackColor = Color.DarkSlateBlue;
+			this.button1.Font = new Font( "Lucida Calligraphy", 10.8F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.button1.ForeColor = Color.Snow;
+			this.button1.Location = new Point( 31, 172 );
+			this.button1.Name = "button1";
+			this.button1.Size = new Size( 87, 38 );
+			this.button1.TabIndex = 57;
+			this.button1.Text = "Off";
+			this.button1.UseVisualStyleBackColor = false;
 			// 
-			// label5
+			// panel8
 			// 
-			this.label5.AutoSize = true;
-			this.label5.BackColor = SystemColors.ButtonFace;
-			this.label5.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label5.Location = new Point( 108, 449 );
-			this.label5.Name = "label5";
-			this.label5.Size = new Size( 357, 84 );
-			this.label5.TabIndex = 5;
-			this.label5.Text = "Here we will probably place a numerical\r\nbar, which will contain the range of the\r\nheat temperature inside the museum.";
+			this.panel8.BackColor = Color.DarkSlateBlue;
+			this.panel8.Location = new Point( 31, 76 );
+			this.panel8.Name = "panel8";
+			this.panel8.Size = new Size( 87, 63 );
+			this.panel8.TabIndex = 56;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.FlatStyle = FlatStyle.Flat;
+			this.label1.Font = new Font( "Lucida Bright", 13.8F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label1.ForeColor = Color.SeaShell;
+			this.label1.Location = new Point( 31, 26 );
+			this.label1.Name = "label1";
+			this.label1.Size = new Size( 91, 26 );
+			this.label1.TabIndex = 55;
+			this.label1.Text = "Light 2";
+			this.label1.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = Color.MediumSlateBlue;
+			this.panel5.Controls.Add( this.button4 );
+			this.panel5.Controls.Add( this.panel6 );
+			this.panel5.Controls.Add( this.label9 );
+			this.panel5.Location = new Point( 3, 3 );
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new Size( 143, 238 );
+			this.panel5.TabIndex = 1;
+			// 
+			// button4
+			// 
+			this.button4.BackColor = Color.DarkSlateBlue;
+			this.button4.Font = new Font( "Lucida Calligraphy", 10.8F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.button4.ForeColor = Color.Snow;
+			this.button4.Location = new Point( 27, 172 );
+			this.button4.Name = "button4";
+			this.button4.Size = new Size( 91, 38 );
+			this.button4.TabIndex = 57;
+			this.button4.Text = "Off";
+			this.button4.UseVisualStyleBackColor = false;
+			// 
+			// panel6
+			// 
+			this.panel6.BackColor = Color.DarkSlateBlue;
+			this.panel6.Location = new Point( 27, 76 );
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new Size( 91, 63 );
+			this.panel6.TabIndex = 56;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.FlatStyle = FlatStyle.Flat;
+			this.label9.Font = new Font( "Lucida Bright", 13.8F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label9.ForeColor = Color.SeaShell;
+			this.label9.Location = new Point( 27, 26 );
+			this.label9.Name = "label9";
+			this.label9.Size = new Size( 91, 26 );
+			this.label9.TabIndex = 55;
+			this.label9.Text = "Light 1";
+			this.label9.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = Color.DarkSlateBlue;
+			this.panel2.Controls.Add( this.panel11 );
+			this.panel2.Location = new Point( 10, 9 );
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new Size( 443, 221 );
+			this.panel2.TabIndex = 0;
+			// 
+			// panel11
+			// 
+			this.panel11.BackColor = Color.MediumSlateBlue;
+			this.panel11.Controls.Add( this.label6 );
+			this.panel11.Controls.Add( this.label5 );
+			this.panel11.Controls.Add( this.button6 );
+			this.panel11.Controls.Add( this.button5 );
+			this.panel11.Controls.Add( this.label4 );
+			this.panel11.Controls.Add( this.button3 );
+			this.panel11.Controls.Add( this.label3 );
+			this.panel11.Location = new Point( 3, 3 );
+			this.panel11.Name = "panel11";
+			this.panel11.Size = new Size( 437, 215 );
+			this.panel11.TabIndex = 58;
 			// 
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.BackColor = SystemColors.ButtonFace;
-			this.label6.Font = new Font( "Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point,   0 );
-			this.label6.Location = new Point( 36, 435 );
+			this.label6.FlatStyle = FlatStyle.Flat;
+			this.label6.Font = new Font( "Lucida Bright", 12F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label6.ForeColor = Color.SeaShell;
+			this.label6.Location = new Point( 238, 149 );
 			this.label6.Name = "label6";
-			this.label6.Size = new Size( 26, 112 );
-			this.label6.TabIndex = 4;
-			this.label6.Text = "H\r\nE\r\nA\r\nT";
+			this.label6.Size = new Size( 33, 23 );
+			this.label6.TabIndex = 62;
+			this.label6.Text = "*C";
+			this.label6.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.FlatStyle = FlatStyle.Flat;
+			this.label5.Font = new Font( "Lucida Bright", 13.8F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label5.ForeColor = Color.DarkSlateBlue;
+			this.label5.Location = new Point( 168, 149 );
+			this.label5.Name = "label5";
+			this.label5.Size = new Size( 74, 26 );
+			this.label5.TabIndex = 61;
+			this.label5.Text = "18.00";
+			this.label5.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// button6
+			// 
+			this.button6.BackColor = Color.DarkSlateBlue;
+			this.button6.Font = new Font( "Lucida Bright", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.button6.ForeColor = Color.Snow;
+			this.button6.Location = new Point( 297, 121 );
+			this.button6.Name = "button6";
+			this.button6.Padding = new Padding( 3, 0, 0, 3 );
+			this.button6.Size = new Size( 69, 54 );
+			this.button6.TabIndex = 60;
+			this.button6.Text = "+";
+			this.button6.UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this.button5.BackColor = Color.DarkSlateBlue;
+			this.button5.Font = new Font( "Lucida Bright", 24F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.button5.ForeColor = Color.Snow;
+			this.button5.Location = new Point( 74, 121 );
+			this.button5.Name = "button5";
+			this.button5.Padding = new Padding( 3, 0, 0, 3 );
+			this.button5.Size = new Size( 69, 54 );
+			this.button5.TabIndex = 59;
+			this.button5.Text = "-";
+			this.button5.UseVisualStyleBackColor = false;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.FlatStyle = FlatStyle.Flat;
+			this.label4.Font = new Font( "Lucida Bright", 12F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label4.ForeColor = Color.SeaShell;
+			this.label4.Location = new Point( 149, 121 );
+			this.label4.Name = "label4";
+			this.label4.Size = new Size( 142, 23 );
+			this.label4.TabIndex = 58;
+			this.label4.Text = "Temperature";
+			this.label4.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// button3
+			// 
+			this.button3.BackColor = Color.DarkSlateBlue;
+			this.button3.Font = new Font( "Lucida Calligraphy", 10.8F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.button3.ForeColor = Color.Snow;
+			this.button3.Location = new Point( 327, 37 );
+			this.button3.Name = "button3";
+			this.button3.Size = new Size( 84, 38 );
+			this.button3.TabIndex = 58;
+			this.button3.Text = "Off";
+			this.button3.UseVisualStyleBackColor = false;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.FlatStyle = FlatStyle.Flat;
+			this.label3.Font = new Font( "Lucida Bright", 16.2F, FontStyle.Bold, GraphicsUnit.Point,   0 );
+			this.label3.ForeColor = Color.SeaShell;
+			this.label3.Location = new Point( 19, 36 );
+			this.label3.Name = "label3";
+			this.label3.Size = new Size( 56, 31 );
+			this.label3.TabIndex = 58;
+			this.label3.Text = "AC";
+			this.label3.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// vScrollBar1
+			// 
+			this.vScrollBar1.Location = new Point( 327, 36 );
+			this.vScrollBar1.Name = "vScrollBar1";
+			this.vScrollBar1.Size = new Size( 26, 141 );
+			this.vScrollBar1.TabIndex = 59;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = Properties.Resources.ArtLogo;
+			this.pictureBox1.Location = new Point( 145, 36 );
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new Size( 147, 141 );
+			this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 60;
+			this.pictureBox1.TabStop = false;
 			// 
 			// DeviceManager
 			// 
 			AutoScaleDimensions = new SizeF( 8F, 20F );
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size( 536, 611 );
+			BackColor = Color.DarkSlateBlue;
+			ClientSize = new Size( 468, 726 );
 			Controls.Add( this.panel1 );
+			ForeColor = Color.DarkSlateBlue;
+			FormBorderStyle = FormBorderStyle.FixedToolWindow;
 			Name = "DeviceManager";
-			Text = "DeviceManager";
+			StartPosition = FormStartPosition.CenterScreen;
 			this.panel1.ResumeLayout( false );
-			this.panel1.PerformLayout();
+			this.panel4.ResumeLayout( false );
+			this.panel13.ResumeLayout( false );
+			this.panel13.PerformLayout();
+			this.panel3.ResumeLayout( false );
+			this.panel9.ResumeLayout( false );
+			this.panel9.PerformLayout();
+			this.panel7.ResumeLayout( false );
+			this.panel7.PerformLayout();
+			this.panel5.ResumeLayout( false );
+			this.panel5.PerformLayout();
+			this.panel2.ResumeLayout( false );
+			this.panel11.ResumeLayout( false );
+			this.panel11.PerformLayout();
+			(( System.ComponentModel.ISupportInitialize )this.pictureBox1).EndInit();
 			this.ResumeLayout( false );
 		}
 
 		#endregion
 
 		private Panel panel1;
-		private Label label1;
-		private Label label2;
+		private Panel panel2;
+		private Panel panel3;
+		private Panel panel4;
+		private Panel panel5;
+		private Label label9;
+		private Panel panel6;
+		private Button button4;
+		private Panel panel11;
+		private Label label3;
+		private Button button3;
+		private Label label4;
+		private Button button6;
+		private Button button5;
 		private Label label5;
 		private Label label6;
-		private Label label3;
-		private Label label4;
+		private Panel panel9;
+		private Button button2;
+		private Panel panel10;
+		private Label label2;
+		private Panel panel7;
+		private Button button1;
+		private Panel panel8;
+		private Label label1;
+		private Panel panel13;
+		private Label label15;
+		private PictureBox pictureBox1;
+		private VScrollBar vScrollBar1;
 	}
 }
