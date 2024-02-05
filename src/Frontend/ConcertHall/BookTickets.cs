4f653a5396a2e4dd994f1820972733f0e924ee49
ConstArt <k.artemiou@outlook.com>
2024-02-05 20:11:32 +0200

    Added form BookTickets.cs in Frontend

---
+++ src/Frontend/ConcertHall/BookTickets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ergasia3.src.Frontend.ConcertHall
+{
+	public partial class BookTickets : Form
+	{
+		public BookTickets()
+		{
+			InitializeComponent();
+		}
+
+	}
+}
